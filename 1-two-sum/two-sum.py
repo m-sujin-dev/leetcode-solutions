c098904b0324d4e80001a66fe2b51938c1ad1a98
m-sujin-dev <msujinofficial@gmail.com>
2026-06-09 21:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (81.94%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_indices = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_indices:
+                return [num_indices[complement], i]
+            num_indices[num] = i
+
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
